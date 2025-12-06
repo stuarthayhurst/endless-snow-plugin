873a7dacc698285b4688a917810ae7eb57999961
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-12-06 18:48:14 +0000

    Tab complete togglesnow command

--- src/main/java/io/github/stuarthayhurst/endlesssnow/ToggleCommand.java
+++ src/main/java/io/github/stuarthayhurst/endlesssnow/ToggleCommand.java
@@ -1,10 +1,13 @@
 package io.github.stuarthayhurst.endlesssnow;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import java.util.Arrays;
+import java.util.List;
 
-public class ToggleCommand implements CommandExecutor {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+public class ToggleCommand implements TabExecutor {
     private EndlessSnow pluginInstance;
 
     ToggleCommand(EndlessSnow instance) {
@@ -28,5 +31,13 @@ public class ToggleCommand implements CommandExecutor {
         }
 
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command,
+                                      String label, String[] args) {
+        List<String> results = Arrays.asList("on", "off");
+
+        return results;
     }
 }
